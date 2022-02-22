50b899d8fa5daa1fdaedfa71ec33be5197b26e01
steampower33 <steampower33@naver.com>
2022-02-22 18:07:17 +0900

    Initial Commit

---
+++ MyFirstApp/MyFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstAppApp.swift
+//  MyFirstApp
+//
+//  Created by SeungMin Lee on 2022/02/22.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
